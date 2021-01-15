3e3c67a02fdf7ec599317b5a979a844afe4ed904
“alexamfrancis” <alexa.m.francis@gmail.com>
2021-01-15 14:50:42 -0800

    searching results

--- Phistar Lowars/ViewController.swift
+++ Phistar Lowars/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        searchBar.delegate = self
     }
 
 
 }
 
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        let results = NetworkManager.shared.people.filter({ $0.name!.lowercased().contains(searchText.lowercased()) })
+        print(results.map({ $0.name }))
+    }
+    
+}
